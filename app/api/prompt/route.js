d625ed35d4b236acfff94914ae824009ee039747
subhajit100 <asubhajit35@gmail.com>
2023-09-11 15:01:07 +0530

    add revalidate=0 to make it dynamic route

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,6 +1,8 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
+export const revalidate = 0;
+
 export const GET = async (req) => {
   try {
     await connectToDB();
